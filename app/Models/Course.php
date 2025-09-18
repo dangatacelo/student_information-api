de90438f7d449d1f50a9296015b8f5b7414d24a2
dangatacelo <dan.gatacelo@sorsu.edu.ph>
2025-09-18 18:39:18 +0800

    Created a new migration and model for Course

---
+++ app/Models/Course.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+  protected $fillable = [
+        'course_code',
+        'course_name',
+        'description',
+        'units',
+        'department',
+        'instructor_id',
+  ];
+}
